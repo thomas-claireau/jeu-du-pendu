8764c0b8ce1332d8aadff63b7c96b4df538e7d08
Thomas Claireau <thomas.claireau@gmail.com>
2020-06-23 08:28:13 +0200

    detect keypress OK / wip : problem has method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
+import autoBind from 'react-autobind';
 import faker from 'faker';
 import './App.css';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
-		this.test();
+		autoBind(this, 'play');
+
+		this.state = {
+			word: this.filterWord(faker.random.word()),
+			usedLetters: new Set(),
+		};
 	}
 
-	test() {
-		faker.locale = 'fr';
-		console.log(faker.random.word());
+	filterWord(word) {
+		return word
+			.toUpperCase()
+			.split('')
+			.filter((item) => item.match(/^[A-Za-z]+$/))
+			.join('');
 	}
 
-	computeDisplay(phrase, usedLetters) {
-		return phrase.replace(/\w/g, (letter) => (usedLetters.has(letter) ? letter : '_'));
+	computeDisplay(word, usedLetters) {
+		console.log(word);
+		console.log(usedLetters);
+		return word.replace(/\w/g, (letter) => (usedLetters.has(letter) ? letter : '_'));
+	}
+
+	// autobind with react autobind
+	play(event) {
+		if (/[A-Za-z]/.test(event.key)) {
+			this.setState((prevState) => prevState.usedLetters.add(event.key));
+		}
+	}
+
+	// add event listener keydown
+	componentDidMount() {
+		document.addEventListener('keydown', this.play, false);
+	}
+
+	// remove event listener keydown
+	componentWillUnmount() {
+		document.removeEventListener('keydown', this.play, false);
 	}
 
 	render() {
-		return <div className="App"></div>;
+		const { word, usedLetters } = this.state;
+		const displayWord = this.computeDisplay(word, usedLetters);
+
+		return (
+			<div className="App">
+				{displayWord.split('').map((item, index) => {
+					if (item === '_') {
+						return <span key={index}>{item}</span>;
+					}
+
+					return item;
+				})}
+			</div>
+		);
 	}
 }
 
